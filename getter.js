ad6b4141749b439953f02e99b553229483fdd220
eksalp <eksalpu0@gmail.com>
2022-12-15 14:05:56 +0700

    first commit

---
+++ getter.js
@@ -0,0 +1,21 @@
+function mapUsers(users) {
+  // TODO: replace this
+  let hasil = {
+    data: users,
+    count: users.length,
+  };
+  return hasil;
+}
+
+function artikel(articles) {
+  // TODO: replace this
+  let hasil = {
+    data: articles,
+    count: articles.length,
+  };
+  return hasil;
+}
+
+// TODO: answer here
+export default mapUsers;
+export const mapArticles = artikel;
